84475308d0c90076d55940aaabd4e3ce9d5b1d9d
agayushh <agayush088@gmail.com>
2025-06-25 16:48:19 +0530

    circle circumference

---
+++ conditional-java/CircleCircum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CircleCircum {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double r = scn.nextDouble();
+        double area = 2* Math.PI * r;
+        System.out.println("circumference of circle is" + area);
+    }
+}
